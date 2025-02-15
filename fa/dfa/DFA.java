d06ef5bbafb869caac19aa2855511a59afda0409
spencer <spencerford674@u.boisestate.edu>
2025-02-14 17:39:31 -0700

    Added more comments to DFA

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -77,47 +77,77 @@ public class DFA implements DFAInterface {
         return false;
     }
 
-    /** Adds existing state to a final state set
-     * @param name - the name of the state to be added to final state set
-     * @return boolean - true - specified state was successfully added to final state set
-     *                 - false - specified state not added to final state set
+    /** Travels the DFA object and return if the passed string is part of the language.
+     * This method will break the string up in to an array of Characters to be compared
+     * to the Characters that make up the names of the states of this DFA object. For every
+     * character of the array this method will see if the state has a valid transition available.
+     * The method will consume the string as long as valid transitions exist until the end when it
+     * will make sure the state that it ends on is a final state.
+     * @param s - the string that is being tested if it is accepted by the DFA object
+     * @return boolean - true - specified string successfully traveled the DFA and landed on a final state
+     *                 - false - specified string failed to find valid transitions to consume the input string
+     *                           or the current state at the end of the input string is not a final state
      **/
     @Override
     public boolean accepts(String s) {
-        DFAState currentState = states.get(startState);
-        for (char c : s.toCharArray()) {
-            if (!sigma.contains(c)) { return false; }
-            String nextState = currentState.transitions.get(c).getName();
-            if (nextState == null) { return false; }
+        DFAState currentState = states.get(startState); //Create a new DFAState object to hold current state
+        for (char c : s.toCharArray()) {                //Break up input string into Character array
+            if (!sigma.contains(c)) { return false; }   //Check that the current Character is in the language
+            String nextState = currentState.transitions.get(c).getName();   //Grab the valid transition
+            if (nextState == null) { return false; }    //Checks if there is a valid transition
 
-            currentState = states.get(nextState);
+            currentState = states.get(nextState);       //Set currentState to state specified by valid transition
             }
-        return finalStates.contains(currentState.getName());
+        return finalStates.contains(currentState.getName()); //Checks currentState is in the final state set
     }
 
+    /** Add a Character to the language
+     * @param symbol - Character to be added to the language
+     **/
     @Override
     public void addSigma(char symbol) {
-        sigma.add(symbol);
+        sigma.add(symbol);  //LinkedHashSets only adds unique symbols
     }
 
+    /** Returns Set of Characters containing the language
+     * @return sigma - Set of Characters containing the language
+     **/
+    @Override
     public Set<Character> getSigma() {
         return sigma;
     }
 
+    /** Returns the State object requested
+     * @param name - the name of the state to be returned
+     * @return State object
+     **/
     public State getState(String name) {
         return states.get(name);
     }
 
+    /** Returns boolean value if specified string is a final state
+     * @param name - the name of the state to be checked for final state status
+     * @return boolean - true - state is a final state
+     *                  false - state is not a final state
+     **/
     @Override
     public boolean isFinal(String name) {
         return finalStates.contains(name);
     }
 
+    /** Returns boolean value if specified string is the start state
+     * @param name - the name of the state to be checked for start state status
+     * @return boolean - true - state is the start state
+     *                  false - state is not the start state
+     **/
     @Override
     public boolean isStart(String name) {
         return startState.equals(name);
     }
 
+    /** Returns string of 5-tuple DFA definition
+     * @return string
+     **/
     @Override
     public String toString() {
         String returnString = "Q = { " + String.join(" ", states.keySet()) + " }\n";
@@ -144,12 +174,22 @@ public class DFA implements DFAInterface {
         return returnString;
     }
 
+    /** Returns boolean value if specified transition is valid
+     * This method will check to make sure fromState, toState are states already in the DFA and the character
+     * is in the language. Then it will add the transition to the fromState transition set.
+     * @param fromState - state transiton is associated with
+     *          toState - state transition is going to
+     *           onSymb - character in the language the transition occurs on
+     * @return boolean - true - transition successfully added
+     *                  false - transition failed to be added
+     **/
     @Override
-    public boolean addTransition(String fromState, String toState, char onSymb) {
+    public boolean addTransition(String fromState, String toState, char onSymb) {   //Checks params are valid
         if ( !states.containsKey(fromState) || !states.containsKey(toState) || !sigma.contains(onSymb)) {
             return false;
         }
-        states.get(fromState).transitions.put(onSymb, states.get(toState));
+        states.get(fromState).transitions.put(onSymb, states.get(toState)); //Add valid transition to fromState
+                                                                            // transition set
         return true;
     }
 
