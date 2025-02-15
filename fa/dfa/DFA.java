c01d82cf1899ff31db366384c60863aae355074e
spencer <spencerford674@u.boisestate.edu>
2025-02-14 18:54:59 -0700

    Javadoc done. Main deleted from DFA

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -8,6 +8,8 @@ import java.util.*;
 /**
  * DFA is an implementation that provides all necessary operations to construct
  * a Determinate Finite Automata.
+ * @author Luis Acosta
+ * @author Spencer Ford
  */
 public class DFA implements DFAInterface {
     protected LinkedHashSet<Character> sigma = new LinkedHashSet<>();
@@ -212,70 +214,6 @@ public class DFA implements DFAInterface {
             return DFACopy;
         }
         return null;
-    }
-
-    public static void main(String[] args) {
-        DFA dfa = new DFA();
-        dfa.addSigma('2');
-        dfa.addSigma('1');
-
-        dfa.addState("G");
-        dfa.addState("D");
-
-        dfa.setFinal("G");
-        dfa.setFinal("D");
-
-        dfa.addState("A");
-        dfa.setStart("D");
-        dfa.setStart("A");
-
-        dfa.addState("B");
-        dfa.addState("C");
-        dfa.addState("E");
-        dfa.addState("F");
-
-        dfa.addState("A");
-        dfa.setFinal("K");
-        dfa.setStart("BK");
-
-        dfa.addTransition("A", "B", '1');
-        dfa.addTransition("A", "C", '2');
-
-        dfa.addTransition("B", "D", '1');
-        dfa.addTransition("B", "E", '2');
-
-        dfa.addTransition("C", "F", '1');
-        dfa.addTransition("C", "G", '2');
-
-        dfa.addTransition("C", "F", '1');
-        dfa.addTransition("C", "G", '2');
-
-        dfa.addTransition("D", "D", '1');
-        dfa.addTransition("D", "E", '2');
-
-        dfa.addTransition("E", "D", '1');
-        dfa.addTransition("E", "E", '2');
-
-        dfa.addTransition("F", "F", '1');
-        dfa.addTransition("F", "G", '2');
-
-        dfa.addTransition("G", "F", '1');
-        dfa.addTransition("G", "G", '2');
-
-
-        dfa.addTransition("FF", "F", '1');
-        dfa.addTransition("F", "GG", '2');
-
-        dfa.addTransition("G", "F", 'K');
-        dfa.addTransition("A", "K", '7');
-
-        System.out.println(dfa.toString());
-
-        DFA dfaCopy = new DFA(dfa.sigma, dfa.states, dfa.finalStates, dfa.startState);
-        dfaCopy = dfaCopy.swap('1', '2');
-
-        System.out.println(dfa.toString());
-        System.out.println(dfaCopy.toString());
     }
 }
 
